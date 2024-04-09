ba02c26cdb16321ba058a6ec2da645a339c04b97
fabmosunitiva <166383290+fabmosunitiva@users.noreply.github.com>
2024-04-09 16:43:31 +0200

    feat: implemented retrieve insurance by targa and retrieve all insurance (#7)
    Co-authored-by: fabmosunitiva <fabiomoscariello@DESKTOP-L0I9SJV>

--- src/main/java/org/acme/MyAssicurazioniService.java
+++ src/main/java/org/acme/MyAssicurazioniService.java
@@ -9,6 +9,7 @@ import org.acme.dto.InsuranceDTO;
 import org.acme.dto.VehicleDTO;
 import org.acme.factory.VeicoloFactory;
 
+import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
@@ -38,4 +39,26 @@ public class MyAssicurazioniService {
         insuranceFiltered.setId(insurance.getId());
         }
 
+    public Insurance getInsurancebyTarga(String targa) throws Exception{
+        Insurance insuranceReturned = null;
+        for (Insurance insuranceIt : listInsurance) {
+            Assicurabile assicurabile = insuranceIt.getAssicurabile();
+            System.out.println(assicurabile);
+            if(assicurabile.getTarga().equalsIgnoreCase(targa)){
+                insuranceReturned = insuranceIt;
+            }
+        }
+        if(insuranceReturned != null){
+            return insuranceReturned;
+        }else{
+            throw new Exception("Nessuna Assicurazione disponibile per quella targa");
+        }
+    }
+
+    public ArrayList<Insurance> getAllInsurances() throws Exception{
+        if(listInsurance.size() <= 0){
+            throw new Exception("Non sono disponibili alcune assicurazioni");
+        }
+        return listInsurance;
+    }
 }
